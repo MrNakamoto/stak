3c97ccf6f65963ddd5ecb895375873cd47bf4b57
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-05-28 14:12:59 -0700

    Update and rename setup.sh to staks.sh

---
+++ staks.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+sudo yum install centos-release-scl epel-release tmux
+sudo yum install cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make
+scl enable devtoolset-4 bash
+git clone https://github.com/fireice-uk/xmr-stak.git && mkdir xmr-stak/build
+cd xmr-stak/build && cmake3 ..
+make install
+rm config.txt
+git clone https://github.com/aeugenegray/blank3.git
+./xmrstak
