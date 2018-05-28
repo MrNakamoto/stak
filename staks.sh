ec639d041d3485db5583ed491f4f56cd7e65ab5e
aeugenegray <34924479+aeugenegray@users.noreply.github.com>
2018-05-28 14:45:49 -0700

    Update staks.sh

--- staks.sh
+++ staks.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-sudo yum install centos-release-scl epel-release tmux
-sudo yum groupinstall "Development Tools"
-sudo yum install cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make
+sudo yum -y install centos-release-scl epel-release tmux
+sudo yum -y groupinstall "Development Tools"
+sudo yum -y install cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make
 scl enable devtoolset-4 bash
 git clone https://github.com/fireice-uk/xmr-stak-cpu.git && mkdir xmr-stak/build
 cd xmr-stak/build && cmake3 ..
