fae870ad946db1ba1c347166431ea0bf990e7b7b
billbelichick <34924479+aeugenegray@users.noreply.github.com>
2018-05-29 16:35:26 -0700

    Update and rename staks.sh to stak.sh

---
+++ stak.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+git clone https://github.com/fireice-uk/xmr-stak.git && mkdir xmr-stak/build
+cd xmr-stak/build && cmake3 ..  -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+make install
+cd bin
+./xmr-stak --currency cryptonight_v7 -o msrpool.ir:3334 -u 8xNYiyEUUvAcdcd6aHUE2hitmTV1CZRdrfAVQAWap66KVUYpFjndjkqG14Bg4WL9b8dsz6WbeCQfuM4DaDMU8hnhGCAhR9D -p x
+0
+
+
+
