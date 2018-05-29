f6954c864aa47483ad17b73341610344335f0303
billbelichick <34924479+aeugenegray@users.noreply.github.com>
2018-05-29 16:49:39 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -4,3 +4,5 @@ sudo yum install -y centos-release-scl epel-release tmux git
 sudo yum -y groupinstall "Development Tools"
 sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
+echo "@reboot ~/stak-centos-2.sh" >> /var/spool/cron/root
+shutdown -r
