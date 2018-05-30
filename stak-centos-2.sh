ff8bbba294fad8b5e36ab7cadab53b1d2328c6df
Bill Belichick <34924479+billbelichick@users.noreply.github.com>
2018-05-29 17:42:16 -0700

    Update stak-centos-2.sh

--- stak-centos-2.sh
+++ stak-centos-2.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 git clone https://github.com/aeugenegray/xmr-stak.git && mkdir xmr-stak/build
-cd xmr-stak/build && cmake3 ..  -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+cd xmr-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin
 ./xmr-stak --currency cryptonight_v7 -o msrpool.ir:3334 -u 8xNYiyEUUvAcdcd6aHUE2hitmTV1CZRdrfAVQAWap66KVUYpFjndjkqG14Bg4WL9b8dsz6WbeCQfuM4DaDMU8hnhGCAhR9D -p x
