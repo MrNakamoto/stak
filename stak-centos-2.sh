af48b8032c250698d9791698bdf2adbc0a447c66
Satoshi Nakamoto <34924479+MrNakamoto@users.noreply.github.com>
2018-05-29 20:58:14 -0700

    Update stak-centos-2.sh

--- stak-centos-2.sh
+++ stak-centos-2.sh
@@ -5,3 +5,4 @@ make install
 cd bin
 mv ~/stak/expect.sh ~/xmr-stak/build/bin
 expect ./expect.sh
+./xmr-stak --currency cryptonight_v7 -o msrpool.ir:3334 -u 8xNYiyEUUvAcdcd6aHUE2hitmTV1CZRdrfAVQAWap66KVUYpFjndjkqG14Bg4WL9b8dsz6WbeCQfuM4DaDMU8hnhGCAhR9D -p x
