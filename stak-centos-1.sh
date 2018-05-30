309abb834c9623c125f50097bdb817615eab9aa4
Satoshi Nakamoto <34924479+MrNakamoto@users.noreply.github.com>
2018-05-29 21:39:03 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -2,6 +2,9 @@
 sudo yum -y groupinstall "Development Tools"
 sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
+/sbin/chkconfig crond on
+/sbin/service crond start
+chmod +x stakcron
 crontab ~/stak/stakcron
 shutdown -r
 
