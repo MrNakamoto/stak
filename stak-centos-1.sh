f492c859a3a7bf79b418b39b286a6ac4582b9230
Satoshi Nakamoto <34924479+MrNakamoto@users.noreply.github.com>
2018-05-31 11:54:52 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 sudo yum -y groupinstall "Development Tools"
-sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make
+sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make vixie-cron crontabs
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
 /sbin/chkconfig crond on
 /sbin/service crond start
