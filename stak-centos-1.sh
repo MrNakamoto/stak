5947deef65c5e08b93cae0de393d56ef7a9b8e85
Satoshi Nakamoto <34924479+MrNakamoto@users.noreply.github.com>
2018-05-31 12:14:33 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -4,6 +4,6 @@ sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel ope
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
 /sbin/chkconfig crond on
 /sbin/service crond start
-./expect-1.sh
+expect ./expect-1.sh
 sudo reboot
 
