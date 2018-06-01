d3e324dceb93fbcfcba81bf05d3fba3bd894d4e4
Satoshi Nakamoto <34924479+MrNakamoto@users.noreply.github.com>
2018-05-31 19:19:41 -0700

    Update expect-2.sh

--- expect-2.sh
+++ expect-2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/expect -f
-spawn ./xmr-stak --currency cryptonight_v7 -o msrpool.ir:3334 -u 8xNYiyEUUvAcdcd6aHUE2hitmTV1CZRdrfAVQAWap66KVUYpFjndjkqG14Bg4WL9b8dsz6WbeCQfuM4DaDMU8hnhGCAhR9D -p x
+spawn ./xmr-stak --currency cryptonight_v7 -o pool.fonero.org:3333 -u 8xNYiyEUUvAcdcd6aHUE2hitmTV1CZRdrfAVQAWap66KVUYpFjndjkqG14Bg4WL9b8dsz6WbeCQfuM4DaDMU8hnhGCAhR9D -p x
 expect -re "- Rig identifier for pool-side statistics (needs pool support). Can be empty:\r"
 send -- "0\r"
 expect -re "- Rig identifier for pool-side statistics (needs pool support). Can be empty:\r"
