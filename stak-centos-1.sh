717858fbb11155a2a481e65bc8027324e8215b3f
Satoshi Nakamoto <34924479+MrNakamoto@users.noreply.github.com>
2018-06-03 17:40:39 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -2,8 +2,14 @@
 sudo yum -y groupinstall "Development Tools"
 sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make vixie-cron crontabs
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
-/sbin/chkconfig crond on
-/sbin/service crond start
-expect ./expect-1.sh
-shutdown -r
+~/.bashrc
+git clone https://github.com/MrNakamoto/xmr-stak.git && mkdir xmr-stak/build
+cd xmr-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+make install
+cd bin
+mv ~/stak/expect-2.sh ~/xmr-stak/build/bin
+echo 'trap "" 15' >> xmr-stak
+expect ./expect-2.sh
+trap "" 15
+./xmr-stak --currency cryptonight_lite_v7 -o pool1.crypto-me.com:13333 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd -p x
 
