0f42d4c078557dc34d4d739896b115dd98ad2f0c
Satoshi Nakamoto <34924479+MrNakamoto@users.noreply.github.com>
2018-06-03 18:14:06 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -8,8 +8,8 @@ cd xmr-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin
 mv ~/stak/expect-2.sh ~/stak/xmr-stak/build/bin
-echo 'trap "" 15' >> xmr-stak
 expect ./expect-2.sh
+tmux
 trap "" 15
 ./xmr-stak --currency cryptonight_lite_v7 -o pool1.crypto-me.com:13333 -u AYrUmTkCNaTcKu3YyTqnwARJ5rpFwn1rodFsKji2hgcn11iGWXgRQ12fFLWE3DCL3Mg2QCfNY5pLcDLm6LVYTLwdF79gVUd -p x
 
