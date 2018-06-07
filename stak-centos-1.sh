5b3a72ed732f33d2555deb9c17793a9dee1cf5f9
A. Eugene Gray <34924479+aeugenegray@users.noreply.github.com>
2018-06-06 23:23:55 -0700

    Update stak-centos-1.sh

--- stak-centos-1.sh
+++ stak-centos-1.sh
@@ -3,11 +3,11 @@ sudo yum -y groupinstall "Development Tools"
 sudo yum install -y cmake3 devtoolset-4-gcc* hwloc-devel libmicrohttpd-devel openssl-devel make vixie-cron crontabs
 echo 'source /opt/rh/devtoolset-4/enable' >> ~/.bashrc
 source ~/.bashrc
-git clone https://github.com/MrNakamoto/xmr-stak.git && mkdir xmr-stak/build
+git clone https://github.com/aeugenegray/xmr-stak.git && mkdir xmr-stak/build
 cd xmr-stak/build && cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install
 cd bin
-mv ~/stak/expect-2.sh ~/stak/xmr-stak/build/bin
+mv ~/xmr-stak-spotme/expect-2.sh ~/xmr-stak-spotme/xmr-stak/build/bin
 expect ./expect-2.sh
 tmux
 trap "" 15
